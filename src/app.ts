91fd6c3e850e3d5a3e0ef436741bd32af6fc9d45
Aishkash <aishwaryakashy@gmail.com>
2025-12-14 01:52:02 +0530

    refactor: Improve Sweet module code structure and readability
    Refactored the Sweet controller and service and improve maintainability. Verified that all existing tests continue to pass after the refactor.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./modules/auth/auth.routes";
+import sweetRoutes from "./modules/sweets/sweets.routes";
 
 dotenv.config();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api", sweetRoutes); 
 
 export default app;
